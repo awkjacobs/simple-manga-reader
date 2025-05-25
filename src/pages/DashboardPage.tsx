040942c870ef5030f3cfb48ec08b728ee98c7e5b
Austin <awkjacobs@gmail.com>
2025-05-25 11:55:14 -0400

    correct e2e test?

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -6,7 +6,12 @@ import React from "react"
 export default function DashboardPage() {
     return (
         <div className="flex flex-col gap-6">
-            <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+            <h1
+                className="text-3xl font-bold tracking-tight"
+                data-testid="pageTitle"
+            >
+                Dashboard
+            </h1>
             <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
                 <ReadingStats />
             </div>
